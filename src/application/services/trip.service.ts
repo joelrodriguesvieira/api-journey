2031fae244c95e4104ef48d2239a3d73d0b5f55f
Joel Rodrigues Vieira <joel.vieira@compasso.com.br>
2024-10-23 10:50:56 -0300

    feature: add validation date in create Trip

--- src/application/services/trip.service.ts
+++ src/application/services/trip.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { TripEntity } from '../../domain/entities/trip.entity';
 import { ParticipantEntity } from '../../domain/entities/participant.entity';
 import { PrismaTripRepository } from '../../infra/data/prisma/repositories/trip.repository';
+import { ClientError } from '../../presentation/errors/clientError';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class TripService {
   constructor(private readonly tripRepository: PrismaTripRepository) {}
 
   async createTrip(tripPayload: TripEntity) {
+    if (dayjs(tripPayload.startsAt).isBefore(new Date())) {
+      throw new ClientError('Invalid trip start date.');
+    }
+
+    if (dayjs(tripPayload.endsAt).isBefore(tripPayload.startsAt)) {
+      throw new ClientError('Invalid trip end date.');
+    }
     let participants: ParticipantEntity[] = [];
     participants = tripPayload.emailsToInvite.map((emailToInvite) => ({
       email: emailToInvite,
@@ -20,7 +29,6 @@ export class TripService {
       isConfirmed: false,
       isOwner: true,
     };
-    console.log(owner);
     participants.push(owner);
     const trip = { ...tripPayload, participants, isConfirmed: false };
     return this.tripRepository.createTrip(trip);
